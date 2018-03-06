e0e8b7462ca12dfcc4b14cf9052eabbdf6659ace
James Nylen <jnylen@gmail.com>
2018-03-05 22:42:52 -0600

    Move time step code to separate file

--- temperature-alarm.ino
+++ temperature-alarm.ino
@@ -19,34 +19,12 @@ SoftwareSerial bpi(PIN_LCD_INPUT, PIN_LCD_OUTPUT, SS_INVERTED);
 
 #define PIN_BUTTON 4
 
-#define MS_TIME_STEP 50
 
-#define STEPS_TEMP_READING 20
 
+// Utility functions
 #include "temperature.h"
 #include "lcd.h"
-
-unsigned long time_step_start = 0;
-unsigned long time_step_max_elapsed = 0;
-uint32_t time_step_counter = 0;
-#define TIME_STEP_WRAP (((uint32_t)20)*3600)
-
-void time_step(bool record_max = true) {
-	time_step_counter = (time_step_counter + 1) % TIME_STEP_WRAP;
-	unsigned long ms = millis();
-	unsigned long elapsed = ms - time_step_start;
-	if (record_max && elapsed > time_step_max_elapsed) {
-		time_step_max_elapsed = elapsed;
-	}
-	if (elapsed >= MS_TIME_STEP) {
-		Serial.println("!");
-	} else if (elapsed > 0) {
-		delay(MS_TIME_STEP - (ms % MS_TIME_STEP));
-	} else {
-		delay(MS_TIME_STEP);
-	}
-	time_step_start = millis();
-}
+#include "time_step.h"
 
 void setup() {
 	digitalWrite(PIN_LED_BUILTIN, LOW);
